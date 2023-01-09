8049832f1b0b9c598be1670d25532299d5b4ec74
BURIN TECHAMA <burin_t@flowaccount.com>
2023-01-09 14:25:14 +0700

    Edit VC

--- cardview2/ViewController.swift
+++ cardview2/ViewController.swift
@@ -9,9 +9,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cardview: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        cardview.layer.cornerRadius = 15
+        cardview.layer.shadowColor = UIColor.black.cgColor
+        cardview.layer.shadowRadius = 10
+        cardview.layer.shadowOpacity = 0.1
+        
+        let gesture = UILongPressGestureRecognizer(target: self, action: #selector(onTapView))
+        gesture.minimumPressDuration = 0
+        cardview.addGestureRecognizer(gesture)
+    }
+    
+    @objc func onTapView(gesture: UITapGestureRecognizer) {
+        switch gesture.state {
+        case .began:
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1) {
+                self.cardview.transform = CGAffineTransform(scaleX: 0.96, y: 0.96)
+            }
+//            UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseOut) {
+//                self.cardview.transform = CGAffineTransform(scaleX: 0.96, y: 0.96)
+//            }
+//        case .changed:
+//            print("very likely, just that the finger wiggled around while the user was holding down the button. generally, just ignore this")
+//        case .possible, .recognized:
+//            print("in almost all cases, simply ignore these two, unless you are creating very unusual custom subclasses")
+//        case .cancelled, .failed, .ended:
+//            cardview.transform = CGAffineTransform.identity
+        default:
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1) {
+                self.cardview.transform = CGAffineTransform.identity
+            }
+//            UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseOut) {
+//                self.cardview.transform = CGAffineTransform.identity
+//            }
+        }
+        
+        
     }
 
 
