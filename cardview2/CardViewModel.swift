b300a72144cab28fa25204968ee74eab39693db4
BURIN TECHAMA <burin_t@flowaccount.com>
2023-01-09 17:54:01 +0700

    edit config yml

--- cardview2/CardViewModel.swift
+++ cardview2/CardViewModel.swift
@@ -10,6 +10,9 @@ import Foundation
 struct CardViewModel {
     
     func calculate(a1: Int, a2: Int) -> Int {
+        let smell1 = 0
+        let smell2 = 1
+        let smell3 = 3
         if a1 > a2 {
             return 1
         }else {
@@ -18,6 +21,9 @@ struct CardViewModel {
     }
     
     func calculateV2(a1: Int, a2: Int) -> Int {
+        let smell1 = 0
+        let smell2 = 1
+        let smell3 = 3
         if a1 == 0 {
             return 10
         }else if a2 == 0 {
